4385b84414708c9601228ab9ed52f415bb22bfca
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-01-05 23:44:09 -0500

    trying to make it look pretty

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,12 @@
-//move the gradient background found in main.css back and forth to give the illusion of infinite scrolling
-var scrollSpeed = 1;
-var current = 0;
-var direction = 'h';
-function bgscroll(){
-    current -= scrollSpeed;
-    $('.gradient').css("background-position", (direction == 'h') ? current+"px 0" : "0 " + current+"px");
+//take the text found in <ul id="readme"> and format it like github's readme Markdown
+//For example, turn #hello into big bold and *text* into italics
+var readme = document.getElementById("readme");
+if (readme) {
+    var text = readme.innerHTML;
+    text = text.replace(/#(.*)/g, "<h1>$1</h1>");
+    text = text.replace(/\*\*(.*)\*\*/g, "<b>$1</b>");
+    text = text.replace(/\*(.*)\*/g, "<i>$1</i>");
+    text = text.replace(/\n/g, "<br>");
+    readme.innerHTML = text;
 }
-setInterval("bgscroll()", 20);
 
-//Import my github repositories and give them a nice box with a link to the repository and a link to the live site
-
-var github = 'https://api.github.com/users/YouthHalo/repos';
-var githubTemplate = $('#github-template').html();
-var compiledGithubTemplate = Handlebars.compile(githubTemplate);
-$.getJSON(github).done(function(data) {
-    var githubData = data;
-    var githubHtml = compiledGithubTemplate(githubData);
-    $('.github').html(githubHtml);
-});
-
-
-//Give my repositories a nice box featuring the readme and a link to the repository
