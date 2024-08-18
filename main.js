33babc3153f1035b174c8958e3d9c57bf9ea7eab
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-18 14:56:31 -0400

    More template stuff

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-//make prev and next buttons cycle between the cards
-//make the cards look like the github projects
-//use api.github.com/users/youthhalo to get my repositories and display them in cards
-
 var github = 'https://api.github.com/users/YouthHalo/repos';
 var githubTemplate = $('#github-template').html();
 var compiledGithubTemplate = Handlebars.compile(githubTemplate);
@@ -9,26 +5,4 @@ $.getJSON(github).done(function(data) {
     var githubData = data;
     var githubHtml = compiledGithubTemplate(githubData);
     $('.github').html(githubHtml);
-});
-
-//make the buttons cycle between the cards
-var card = $('.card');
-var prev = $('#prev');
-var next = $('#next');
-var cardCount = 0;
-prev.on('click', function() {
-  cardCount--;
-  if (cardCount < 0) {
-    cardCount = card.length - 1;
-  }
-  card.hide();
-  card.eq(cardCount).show();
-});
-next.on('click', function() {
-  cardCount++;
-  if (cardCount > card.length - 1) {
-    cardCount = 0;
-  }
-  card.hide();
-  card.eq(cardCount).show();
 });
