1ac428d1a852a525ed5272792d2ce3dff1643ac6
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-18 17:39:10 -0400

    Flex refuses to work properly with lists

--- main.js
+++
@@ -1,8 +0,0 @@
-var github = 'https://api.github.com/users/YouthHalo/repos';
-var githubTemplate = $('#github-template').html();
-var compiledGithubTemplate = Handlebars.compile(githubTemplate);
-$.getJSON(github).done(function(data) {
-    var githubData = data;
-    var githubHtml = compiledGithubTemplate(githubData);
-    $('.github').html(githubHtml);
-});
