2368645fd7584b2a560a8322ac0d17e394c562cc
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-31 10:22:37 -0400

    Projects now need to be clicked to enlarge

--- main.js
+++ main.js
@@ -13,21 +13,43 @@ if (n >= 5 && n < 12) {
 
 document.querySelector("h1").innerHTML = greeting + " I'm";
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 var topnav = document.getElementById("topnav");
 function scrollFunction() {
-  if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) { //the first condition seems to do nothing
+  if (
+    document.body.scrollTop > 450 ||
+    document.documentElement.scrollTop > 450
+  ) {
+    //the first condition seems to do nothing
     topnav.style.top = "0";
   } else {
     topnav.style.top = "-4vw";
   }
 }
-document.querySelectorAll('a[href="/"]').forEach(function(a) {
-  a.addEventListener('click', function(e) {
+document.querySelectorAll('a[href="/"]').forEach(function (a) {
+  a.addEventListener("click", function (e) {
     e.preventDefault();
     window.scrollTo({
       top: 0,
-      behavior: 'smooth' // or 'auto', which defaults to instant or 'instant'
+      behavior: "smooth", // or 'auto', which defaults to instant or 'instant'
     });
+  });
+});
+
+document.querySelectorAll('.project').forEach(function (project) {
+  project.addEventListener('click', function (e) {
+    document.querySelectorAll('.project').forEach(function (p) {
+      p.classList.remove('clicked');
+    });
+    if (project.classList.contains('clicked')) {
+      project.classList.remove('clicked');
+    } else {
+      project.classList.add('clicked');
+    }
+  });
+  project.addEventListener('mouseleave', function (e) {
+    project.classList.remove('clicked');
   });
 });
