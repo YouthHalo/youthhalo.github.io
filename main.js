c73a40659af51533216ab180624d13e284403b1f
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-09-17 17:14:49 -0400

    Just realised I was indenting with spaces ;-; changed to tabs

--- main.js
+++ main.js
@@ -1,63 +1,63 @@
 window.scrollTo(0, 0);
 window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
+	window.scrollTo(0, 0);
 };
 
 var d = new Date();
 var n = d.getHours();
 var greeting;
 if (n >= 5 && n < 12) {
-  greeting = "Good morning!";
+	greeting = "Good morning!";
 } else if (n >= 12 && n < 18) {
-  greeting = "Good afternoon!";
+	greeting = "Good afternoon!";
 } else if (n >= 18 && n < 21) {
-  greeting = "Good evening!";
+	greeting = "Good evening!";
 } else {
-  greeting = "Good night!";
+	greeting = "Good night!";
 }
 
 document.querySelector("h1").innerHTML = greeting + " I'm";
 
 window.onscroll = function () {
-  scrollFunction();
+	scrollFunction();
 };
 var topnav = document.getElementById("topnav");
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 450 ||
-    document.documentElement.scrollTop > 450
-  ) {
-    //the first condition seems to do nothing
-    topnav.style.top = "0";
-  } else {
-    topnav.style.top = "-4vw";
-  }
+	if (
+		document.body.scrollTop > 450 ||
+		document.documentElement.scrollTop > 450
+	) {
+		//the first condition seems to do nothing
+		topnav.style.top = "0";
+	} else {
+		topnav.style.top = "-4vw";
+	}
 }
 
 document.querySelectorAll('a[href="/"]').forEach(function (a) {
-  a.addEventListener("click", function (e) {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth", // or 'auto', which defaults to instant or 'instant'
-    });
-  });
+	a.addEventListener("click", function (e) {
+		e.preventDefault();
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth", // or 'auto', which defaults to instant or 'instant'
+		});
+	});
 });
 
 document.querySelectorAll(".project").forEach(function (project) {
-  project.addEventListener("click", function (e) {
-    document.querySelectorAll(".project").forEach(function (p) {
-      //p.classList.remove('clicked'); removing this helps with the clicking on the same project
-    });
-    if (project.classList.contains("clicked")) {
-      project.classList.remove("clicked");
-    } else {
-      project.classList.add("clicked");
-    }
-  });
-  project.addEventListener("mouseleave", function (e) {
-    project.classList.remove("clicked");
-  });
+	project.addEventListener("click", function (e) {
+		document.querySelectorAll(".project").forEach(function (p) {
+			//p.classList.remove('clicked'); removing this helps with the clicking on the same project
+		});
+		if (project.classList.contains("clicked")) {
+			project.classList.remove("clicked");
+		} else {
+			project.classList.add("clicked");
+		}
+	});
+	project.addEventListener("mouseleave", function (e) {
+		project.classList.remove("clicked");
+	});
 });
 
 const repoContainer = document.getElementById("repos");
@@ -66,78 +66,77 @@ let scrollSpeed = 1.5;
 let scrollSpeedDecimalTracker = 0;
 
 window.addEventListener("load", () => {
-  self.setInterval(() => {
-    const scrollLimit = document.documentElement.clientWidth * 2.213;
-    if (repoContainer.scrollLeft > scrollLimit) {
-      repoContainer.scrollTo(0, 0);
-    } else if (scrollSpeed > 1) {
-      repoContainer.scrollTo(repoContainer.scrollLeft + scrollSpeed, 0);
-    } else {
-      if (scrollSpeedDecimalTracker >= 1) {
-        repoContainer.scrollTo(
-          repoContainer.scrollLeft + Math.floor(scrollSpeedDecimalTracker),
-          0
-        );
-        scrollSpeedDecimalTracker =
-          scrollSpeedDecimalTracker - Math.floor(scrollSpeedDecimalTracker);
-      } else {
-        scrollSpeedDecimalTracker = scrollSpeedDecimalTracker + scrollSpeed;
-      }
-    }
-  }, 15);
+	self.setInterval(() => {
+		const scrollLimit = document.documentElement.clientWidth * 2.213;
+		if (repoContainer.scrollLeft > scrollLimit) {
+			repoContainer.scrollTo(0, 0);
+		} else if (scrollSpeed > 1) {
+			repoContainer.scrollTo(repoContainer.scrollLeft + scrollSpeed, 0);
+		} else {
+			if (scrollSpeedDecimalTracker >= 1) {
+				repoContainer.scrollTo(
+					repoContainer.scrollLeft + Math.floor(scrollSpeedDecimalTracker),
+					0
+				);
+				scrollSpeedDecimalTracker =
+					scrollSpeedDecimalTracker - Math.floor(scrollSpeedDecimalTracker);
+			} else {
+				scrollSpeedDecimalTracker = scrollSpeedDecimalTracker + scrollSpeed;
+			}
+		}
+	}, 15);
 });
 
 repoContainer.addEventListener("mouseenter", () => {
-  scrollSpeed = 0.75;
+	scrollSpeed = 0.75;
 });
 
 repoContainer.addEventListener("mouseleave", () => {
-  scrollSpeed = 1.5;
+	scrollSpeed = 1.5;
 });
 
 /*
 document.querySelectorAll('a[href="#about"]').forEach(function (a) {
-  a.addEventListener("click", function (e) {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth", // or 'auto', which defaults to instant or 'instant'
-    });
-  });
+	a.addEventListener("click", function (e) {
+	e.preventDefault();
+	window.scrollTo({
+	top: 0,
+		behavior: "smooth", // or 'auto', which defaults to instant or 'instant'
+	});
+	});
 });
 
 document.querySelectorAll('a[href="#projects"]').forEach(function (a) {
   a.addEventListener("click", function (e) {
-    e.preventDefault();
-    const h2 = document.querySelector('h2:has-text("Projects")');
-    window.scrollTo({
-      top: h2.offsetTop,
-      behavior: "smooth", // or 'auto', which defaults to instant or 'instant'
-    });
+	e.preventDefault();
+	const h2 = document.querySelector('h2:has-text("Projects")');
+	window.scrollTo({
+	  top: h2.offsetTop,
+	  behavior: "smooth", // or 'auto', which defaults to instant or 'instant'
+	});
   });
 });
 
 document.querySelectorAll('a[href="#repos"]').forEach(function (a) {
   a.addEventListener("click", function (e) {
-    e.preventDefault();
-    const contactSection = document.querySelector(".repos");
-    window.scrollTo({
-      top: h2.offsetTop,
-      behavior: "smooth", // or 'auto', which defaults to instant or 'instant'
-    });
+	e.preventDefault();
+	const contactSection = document.querySelector(".repos");
+	window.scrollTo({
+	top: h2.offsetTop,
+	  behavior: "smooth", // or 'auto', which defaults to instant or 'instant'
+	});
   });
 }); */ //these dont do anything yet
 
-
 document.addEventListener("mousedown", (e) => {
-  const circle = document.createElement("div");
-  circle.className = "cursor-circle";
-  circle.style.position = "absolute";
-  circle.style.left = e.clientX + "px";
-  circle.style.top = e.clientY + "px";
-  document.body.appendChild(circle);
-  setTimeout(() => {
-    circle.remove();
-  }, 250);
+	const circle = document.createElement("div");
+	circle.className = "cursor-circle";
+	circle.style.position = "absolute";
+	circle.style.left = e.clientX + "px";
+	circle.style.top = e.clientY + "px";
+	document.body.appendChild(circle);
+	setTimeout(() => {
+		circle.remove();
+	}, 250);
 });
 //doesnt do what i want it to dooo
